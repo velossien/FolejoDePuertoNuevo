dce4e457c911a66cc87d242c824d9ab035d12562
velossien <tararochegrimm@gmail.com>
2017-01-29 21:48:41 -0500

    added about section - not styled yet

--- app/components/Main.js
+++ app/components/Main.js
@@ -11,7 +11,7 @@ class Main extends React.Component {
         return (
             <div className='mainContainer'>
                 <header>
-                    <div className="artistBlock">
+                    <div className="artistBlock"   id="Gallery">
                         <img className="artistImg" src="dist/img/artistImgBrown.png"/>
                         <h1> KEVIN GRIMM </h1>
                         <br/>
@@ -23,12 +23,29 @@ class Main extends React.Component {
                         <Sticky>   
                             <div className="menubar">
                                 <ul>
-                                    <li><a href="index.html">Portfolio</a></li>
-                                    <li><a href="index.html">About</a></li>
+                                    <li><a href="#Gallery">Portfolio</a></li>
+                                    <li><a href="#About">About</a></li>
                                 </ul>
                             </div> 
                         </Sticky>
+                        
                         <ImageGallery/>
+                        
+                        <div className="About" id="About">
+                            <h1> Summary </h1>
+
+                            <p>Hello, I am a Concept and Character artist. I have worked as an animator, an illustrator and a concept artist. Currently I am working as an artist in the game industry.</p>
+                            <p>I am constantly working to improve my technique. I love the challenges that freelance brings me. Whether it's creating a unique character design, a vast environmental game world or cartoons for a children's book, It's all fun to me and doing great work is what I'm passionate about.</p>
+
+                            <h1>Skills</h1>
+                            <ul>
+                                <li>Character Design</li>
+                                <li>Concept Art</li>
+                            </ul>
+                        
+                            <h1> Contact </h1>
+                            <p>Email: <a href="mailto:kevingrimmartist@gmail.com">kevingrimmartist@gmail.com</a></p>
+                        </div>
                 </StickyContainer>
             </div>
 
