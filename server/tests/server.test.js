ec29de353d7cec8c61ce4839f835fe6a017dc3f3
Tara Grimm <tararochegrimm@gmail.com>
2017-10-04 15:26:43 -0400

    Added tests for POST /images, GET /images and GET /images:id routes.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -42,4 +42,91 @@ describe("POST /images", () => {
                 }).catch((error) => done(error));
             });
     });
+
+    it("should not create a image with no src or lightbox src", (done) => {
+
+        request(app)
+            .post("/images")
+            .set("x-auth", users[0].tokens[0].token)
+            .send({
+                src: "",
+                lightboxImage: {
+                    src: ""
+                }
+            })
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Image.find().then((images) => {
+                    expect(images.length).toBe(2);
+                    done();
+                }).catch((error) => done(error));
+            });
+    });
 });
+
+describe("GET /images", () => {
+
+    it("should get all the images by a certain user", (done) => {
+        request(app)
+            .get("/images")
+            .set("x-auth", users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.images.length).toBe(1);
+            })
+            .end(done);
+    });
+
+    it("should not get images if no auth-x token (if not logged in)", (done) => {
+        request(app)
+            .get("/images")
+            .expect(401)
+            .end(done)
+    });
+});
+
+describe("GET /images/:id", () => {
+
+    it("should find a specific image by its id", (done) => {
+
+        request(app)
+            .get(`/images/${images[0]._id.toHexString()}`)
+            .set("x-auth", users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.image.src).toBe(images[0].src);
+            })
+            .end(done);
+    });
+
+    it("should not return an image if created by another user", (done) => {
+        request(app)
+            .get(`/images/${images[0]._id.toHexString()}`)
+            .set("x-auth", users[1].tokens[0].token)
+            .expect(404)
+            .end(done);
+    });
+
+    it("should return 404 if the id given is a non-object id", (done) => {
+        request(app)
+            .get("/images/123")
+            .set("x-auth", users[0].tokens[0].token)
+            .expect(404)
+            .end(done);
+    });
+
+    it("should return 404 if image not found", (done) => {
+        
+                let testID = new ObjectID();
+        
+                request(app)
+                    .get(`/images/${testID.toHexString()}`)
+                    .set("x-auth", users[0].tokens[0].token)
+                    .expect(404)
+                    .end(done);
+            });
+});
+
