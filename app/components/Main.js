53183b981a7bf632e6ecaaf486be12fa8b9fc706
Tara Grimm <tararochegrimm@gmail.com>
2017-10-27 18:09:41 -0400

    Cleaning up the URL a bit

--- app/components/Main.js
+++ app/components/Main.js
@@ -12,12 +12,12 @@ class Main extends React.Component {
             <div className='mainContainer'>
                 <header>
                     <div className="artistBlock">
-                        <Link to="/About"><img className="artistImg" src="https://s3.amazonaws.com/kevingrimm.com/img/artistImg.png"/></Link>
+                        <Link to="/About"><img className="artistImg" src="img/artistImg.png"/></Link>
                         <h1> KEVIN GRIMM </h1>
                         <br/>
                         <h3> Environmental concept artist and character designer </h3>
                     </div>
-                        <img className="banner" src='https://s3.amazonaws.com/kevingrimm.com/img/banner.jpg'/>
+                        <img className="banner" src='img/banner.jpg'/>
                 </header>
                 <StickyContainer>
                         <Sticky>   
--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 require('./styles/scss/style.scss');
 import Main from './components/Main';
-import ImageGallery from './components/ImageGallery';
+import Gallery from './components/Gallery';
 import { StickyContainer, Sticky } from 'react-sticky';
 import routes from './config/routes';
 
