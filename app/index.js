e37520e48952fe055d59f9044bd57631b53cd853
Tara Grimm <tararochegrimm@gmail.com>
2017-10-27 21:03:04 -0400

    Tweaked webpack config to create a production build of the site.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 require('./styles/scss/style.scss');
 import Main from './components/Main';
 import Gallery from './components/Gallery';
 import { StickyContainer, Sticky } from 'react-sticky';
 import routes from './config/routes';
+
+if (process.env.NODE_ENV !== "production") {
+    console.log('Looks like we are in development mode!');
+}
 
 ReactDOM.render(routes, document.getElementById('app'));
