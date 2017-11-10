2118a0c6c3fa2aa5b7f463736c56ebec8472af24
Tara Grimm <tararochegrimm@gmail.com>
2017-11-09 19:46:17 -0500

    Small change to imports in index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 require('./styles/scss/style.scss');
-import Main from './components/Main.jsx';
-import Gallery from './components/Gallery.jsx';
-import { StickyContainer, Sticky } from 'react-sticky';
 import routes from './config/Routes.jsx';
 
 if (process.env.NODE_ENV !== "production") {
