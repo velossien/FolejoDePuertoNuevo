977ae508507ebf3e889d539d8e1d00ed25290656
Tara Grimm <tararochegrimm@gmail.com>
2019-05-06 14:19:45 -0700

    Updating Repo. Add web-dev-server.

--- server/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require("mongoose");
-
-mongoose.Promise = global.Promise;  //allows us to use promises
-
-mongoose.connect(process.env.MONGODB_URI,{useMongoClient: true}); //connects to the database called KGWeb
-
-module.exports = { mongoose };
